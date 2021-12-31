566105e50319e5de63f2386d8ad6d748ecf31a4a
skalexey <skorohodov-ad@narod.ru>
2021-12-31 18:34:37 +0300

    'Core' subdirectory created. Qt project moved into a separated 'Client' directory. Build.py, Clean.py scripts created for Core CMake project

---
+++ Core/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+
+	std::cout << "DataModelBuilderCore project entry point reached\n";
+	if (argc > 1)
+		std::cout << "You say \"" << argv[1] << "\"?";
+	return 0;
+}
