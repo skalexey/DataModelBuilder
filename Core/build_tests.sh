911a71308e1ca483b3cadaefc4c8c7c117987c98
skalexey <skorohodov-ad@narod.ru>
2022-01-12 14:08:55 +0300

    Added build_tests.sh script for building and running the tests

---
+++ Core/build_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+onlyRun=false
+
+for var in "$@"
+do
+	if [ "$var" = "run" ] ; then
+		onlyRun=true
+	fi
+done
+if ! $onlyRun ; then
+	echo "--- Build the project"
+	python clean.py
+	python build.py "$@"
+fi
+./Build/Debug/DMBTest.exe
+
