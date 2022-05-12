8ed441953407ef762bc1958ac4ffc71de8a5a102
skalexey <skorohodov-ad@narod.ru>
2022-05-12 10:31:57 +0300

    Scripts update: added log function, args parsing moved to build_utils script

--- Core/deps_scenario.sh
+++ Core/deps_scenario.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 source dependencies.sh
 source deps_config.sh
 
 download_dependency "VL" "$depsLocation" "git@github.com:skalexey/VL.git"
+curentDir=${PWD}
+
+cd $currentDir
--- Core/rebuild.sh
+++ Core/rebuild.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 python clean.py
 [ $? -ne 0 ] && echo "--- Error during clean process" || echo "--- Clean done"
