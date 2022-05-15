10b479676cc208935661651e95994f80f3653464
Alex <skorohodov-ad@narod.ru>
2022-05-15 21:50:18 +0300

    Update build scripts

--- Core/deps_scenario.sh
+++ Core/deps_scenario.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-source dependencies.sh
-source deps_config.sh
+deps_scenario()
+{
+	source dependencies.sh
+	source deps_config.sh
 
-download_dependency "VL" "$depsLocation" "git@github.com:skalexey/VL.git"
-curentDir=${PWD}
+	download_dependency "VL" "$depsLocation" "git@github.com:skalexey/VL.git"
+	local curentDir=${PWD}
 
-cd $currentDir
+	cd $currentDir
+}
+
+deps_scenario $@
