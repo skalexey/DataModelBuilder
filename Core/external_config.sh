c433c59354ac14d38903c73158c7b1c739d3fd4c
Alex <alexeiskorokhodov@gmail.com>
2022-09-04 12:00:10 +0300

    Update deps configuration.

--- Core/external_config.sh
+++ Core/external_config.sh
@@ -3,7 +3,7 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
 
 if is_windows; then
-	export Core_deps="${HOME}/Projects"
+	export dmbcore_deps="${HOME}/Projects"
 else
-	export Core_deps="${HOME}/Projects"
+	export dmbcore_deps="${HOME}/Projects"
 fi
